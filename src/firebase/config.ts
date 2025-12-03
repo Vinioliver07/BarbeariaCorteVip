b48fa3ccc8d8a4756fb84299165f2e145d50d0ea
VINNII OLIVER <cretinando@gmail.com>
2025-12-03 21:02:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5687123868-61f5f",
+  "appId": "1:79642801752:web:36f252c87754f9e465fc2d",
+  "apiKey": "AIzaSyBktpxXlrTU7X8cw_Eg_K6KPu5w7kwrIn4",
+  "authDomain": "studio-5687123868-61f5f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "79642801752"
+};
